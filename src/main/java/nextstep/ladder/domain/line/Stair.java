9825638ba946e556dbd857ac2a09f7a07ca432a5
ohtaeg <otk1090@naver.com>
2020-05-03 02:34:50 +0900

    refactor (Stair) : expression 추가

--- src/main/java/nextstep/ladder/domain/line/Stair.java
+++ src/main/java/nextstep/ladder/domain/line/Stair.java
@@ -1,18 +1,24 @@
 package nextstep.ladder.domain.line;
 
 public enum Stair {
-    EXIST(true),
-    NOT_EXIST(false)
+    EXIST(true, "-----"),
+    NOT_EXIST(false, "     ")
     ;
 
     private final boolean isExist;
+    private final String expression;
 
-    Stair(final boolean isExist) {
+    Stair(final boolean isExist, final String expression) {
         this.isExist = isExist;
+        this.expression = expression;
     }
 
     public boolean isExist() {
         return isExist;
+    }
+
+    public String getExpression() {
+        return expression;
     }
 
     public static Stair find(boolean isExist) {
