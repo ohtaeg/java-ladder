cbd8eac8fb0efb86254eb2853614852fe64bbe75
ohtaeg <otk1090@naver.com>
2020-05-02 18:55:36 +0900

    refactor (LineGenerateStrategy) : 람다로 변경할 수 있도록 리팩토링

--- src/main/java/nextstep/ladder/domain/line/LineGenerateStrategy.java
+++ src/main/java/nextstep/ladder/domain/line/LineGenerateStrategy.java
@@ -2,6 +2,7 @@ package nextstep.ladder.domain.line;
 
 import java.util.List;
 
+@FunctionalInterface
 public interface LineGenerateStrategy {
     List<Stair> generate(final int playerNameCount);
 }
