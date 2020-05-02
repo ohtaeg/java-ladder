bad1627089bee3223f49f7e2d4483b531560b12d
ohtaeg <otk1090@naver.com>
2020-05-02 18:55:00 +0900

    test (Line) : 연속된 라인 테스트 코드 추가, 테스트시 Strategy를 람다로 변경

--- src/test/java/nextstep/ladder/domain/line/LineTest.java
+++ src/test/java/nextstep/ladder/domain/line/LineTest.java
@@ -1,24 +1,23 @@
 package nextstep.ladder.domain.line;
 
-import nextstep.ladder.domain.line.fake.FakeRandomStrategy;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LineTest {
     private LineGenerateStrategy lineGenerateStrategy;
 
-    @BeforeEach
-    void setUp() {
-        lineGenerateStrategy = new FakeRandomStrategy();
-    }
-
     @DisplayName("참여자가 1명이라면 첫번째 Stair은 존재하지 않는다.")
     @Test
     void isNotExist_OnePlayer() {
         int playerCount = 1;
+        lineGenerateStrategy = playerNameCount -> new ArrayList<>(Arrays.asList(Stair.NOT_EXIST));
         Line line = Line.generate(lineGenerateStrategy, playerCount);
 
         Stair actual = line.stream()
@@ -26,5 +25,33 @@ class LineTest {
                            .orElse(Stair.NOT_EXIST);
 
         assertThat(actual.isExist()).isFalse();
+    }
+
+    @DisplayName("참여자가 2명일때 두번째 Stair은 존재한다.")
+    @Test
+    void existSecondStair() {
+        int playerCount = 2;
+        lineGenerateStrategy = playerNameCount -> new ArrayList<>(Arrays.asList(Stair.NOT_EXIST, Stair.EXIST));
+        Line line = Line.generate(lineGenerateStrategy, playerCount);
+
+        final List<Stair> stairs = line.stream()
+                                       .collect(Collectors.toList());
+
+        assertThat(stairs.get(0).isExist()).isFalse();
+        assertThat(stairs.get(1).isExist()).isTrue();
+    }
+
+    @DisplayName("참여자가 2명일때 두번째 Stair은 존재하지 않는다..")
+    @Test
+    void notExistSecondStair() {
+        int playerCount = 2;
+        lineGenerateStrategy = playerNameCount -> new ArrayList<>(Arrays.asList(Stair.NOT_EXIST, Stair.NOT_EXIST));
+        Line line = Line.generate(lineGenerateStrategy, playerCount);
+
+        final List<Stair> stairs = line.stream()
+                                       .collect(Collectors.toList());
+
+        assertThat(stairs.get(0).isExist()).isFalse();
+        assertThat(stairs.get(1).isExist()).isFalse();
     }
 }
