ab2713b859c36ecbb257d27d42ae3b592600d2b4
ohtaeg <otk1090@naver.com>
2020-05-02 17:07:09 +0900

    test (InputView) : InputView test, 플레이어 이름과 높이 입력받는 테스트 코드 추가

--- src/test/java/nextstep/ladder/view/InputViewTest.java
+++ src/test/java/nextstep/ladder/view/InputViewTest.java
@@ -1,5 +1,7 @@
 package nextstep.ladder.view;
 
+import nextstep.ladder.domain.height.Height;
+import nextstep.ladder.domain.player.PlayerNames;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -13,13 +15,26 @@ class InputViewTest {
 
     @DisplayName("플레이어 이름을 받을 수 있다.")
     @Test
-    void inputPlayers() {
+    void inputPlayerNames() {
         final String names = "pobi,honux,crong,jk";
         inputView = new InputView(init(names));
 
-        String actual = inputView.inputPlayers();
+        PlayerNames actual = inputView.inputPlayerNames();
 
-        assertThat(actual).isEqualTo(names);
+        assertThat(actual.size()).isEqualTo(4);
+    }
+
+    @DisplayName("높이를 입력받는다.")
+    @Test
+    void inputHeight() {
+        final String value = "5";
+        final int intValue = 5;
+        inputView = new InputView(init(value));
+        Height expect = Height.valueOf(intValue);
+
+        Height actual = inputView.inputHeight();
+
+        assertThat(actual).isEqualTo(expect);
     }
 
     InputStream init(String value) {
